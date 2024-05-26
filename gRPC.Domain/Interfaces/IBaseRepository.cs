ea807f9bd9d6c38cc026bc5c3f152a2bd9b47360
Rax <Rax@DESKTOP-41U8OV7>
2024-05-26 19:42:50 +0300

    [gRPC.Domain] Add base repository interface

---
+++ gRPC.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace gRPC.Domain.Interfaces
+{
+    internal interface IBaseRepository<T> where T : class
+    {
+        Task Create(T entity, CancellationToken cancellationToken);
+
+        IQueryable<T> Select();
+
+        Task Delete(T entity, CancellationToken cancellationToken);
+
+        Task Update(T entity, CancellationToken cancellationToken);
+    }
+}
