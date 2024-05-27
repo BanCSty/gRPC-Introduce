88f94a02a5f34d6f672d21ddfe8be79a3e388594
Rax <Rax@DESKTOP-41U8OV7>
2024-05-27 12:13:00 +0300

    [gRPC.Application] Add DI for grpc.Service pipline

---
+++ gRPC.Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+using FluentValidation;
+using gRPC.Application.Common.Behaviors;
+
+namespace gRPC.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            //регистрирует MediatR в контейнере зависимостей и настраивает все необходимые типы,
+            //связанные с медиатором, включая обработчики запросов и команд
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services
+                //добавляет валидаторы в контейнер зависимостей
+                .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+
+            //регистрирует ValidationBehavior<,> как общее поведение для всех типов запросов и команд, реализующих интерфейс
+            //IPipelineBehavior<,>. Это поведение будет применять валидацию к запросам и командам перед их выполнением
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
