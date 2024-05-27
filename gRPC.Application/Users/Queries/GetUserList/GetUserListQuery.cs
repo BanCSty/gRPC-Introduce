d0909aefee93919006d2cd4f87040aee11b6bab5
Rax <Rax@DESKTOP-41U8OV7>
2024-05-27 12:12:22 +0300

    [gRPC.Application] Add get user list query

---
+++ gRPC.Application/Users/Queries/GetUserList/GetUserListQuery.cs
@@ -0,0 +1,10 @@
+﻿using gRPC.Domain;
+using MediatR;
+
+namespace gRPC.Application.Users.Queries.GetUserList
+{
+    public class GetUserListQuery : IRequest<List<User>>
+    {
+
+    }
+}
---
+++ gRPC.Application/Users/Queries/GetUserList/GetUserListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using gRPC.Domain;
+using gRPC.Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace gRPC.Application.Users.Queries.GetUserList
+{
+    public class GetUserListQueryHandler : IRequestHandler<GetUserListQuery, List<User>>
+    {
+        private readonly IBaseRepository<User> _userRepository;
+
+        public GetUserListQueryHandler(IBaseRepository<User> founderRepository)
+        {
+            _userRepository = founderRepository;
+        }
+
+        public async Task<List<User>> Handle(GetUserListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _userRepository.Select()
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+
+            return entity;
+        }
+    }
+}
