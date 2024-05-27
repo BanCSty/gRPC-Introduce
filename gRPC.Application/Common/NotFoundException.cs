51a3bcd545835bcb915a1c420596491a1d265fa7
Rax <Rax@DESKTOP-41U8OV7>
2024-05-27 12:09:19 +0300

    [gRPC.Application] Add validator and custom [NotFoundException]

---
+++ gRPC.Application/Common/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace gRPC.Application.Common
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) not found.") { }
+    }
+}
